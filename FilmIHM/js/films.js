84f6e4bfa400b95d13f0b5e02b255bf36697d0a9
Aurélien <colombet.aurelien@etu.unice.fr>
2016-10-24 14:18:29 +0200

    first commit with a lot of things done

---
+++ FilmIHM/js/films.js
@@ -0,0 +1,38 @@
+var films = [
+  {
+    id : 1,
+    name : "Star Wars IV : Un nouvel espoir",
+    date : "1977",
+    duree : "2h01m",
+    genres : ["SF"],
+    realisateur : "George Lucas",
+    acteurs : ["Mark Hamill", "Harrison Ford", "Carrie Fisher"],
+    description:"Il y a bien longtemps, dans une galaxie très lointaine... La guerre civile fait rage entre l'Empire galactique et l'Alliance rebelle. Capturée par les troupes de choc de l'Empereur menées par le sombre et impitoyable Dark Vador, la princesse Leia Organa dissimule les plans de l'Etoile Noire, une station spatiale invulnérable, à son droïde R2-D2 avec pour mission de les remettre au Jedi Obi-Wan Kenobi. Accompagné de son fidèle compagnon, le droïde de protocole C-3PO, R2-D2 s'échoue sur la planète Tatooine et termine sa quête chez le jeune Luke Skywalker. Rêvant de devenir pilote mais confiné aux travaux de la ferme, ce dernier se lance à la recherche de ce mystérieux Obi-Wan Kenobi, devenu ermite au coeur des montagnes désertiques de Tatooine... ",
+    note : "5",
+    image : "star-wars-episode-4.jpg"
+  },
+  {
+    id:2,
+    name : "Orange Mécanique",
+    date : "1972",
+    duree : "2h16m",
+    genres : ["SF","Drame"],
+    realisateur : "Stanley Kubrick",
+    acteurs : [ "Malcolm McDowell", "Patrick Magee", "Michael Bates"],
+    description:"Au XXIème siècle, où règnent la violence et le sexe, Alex, jeune chef de bande, exerce avec sadisme une terreur aveugle. Après son emprisonnement, des psychanalystes l'emploient comme cobaye dans des expériences destinées à juguler la criminalité... ",
+    note : "4",
+    image : "A_Clockwork_Orange.jpg"
+  },
+  {
+    id:3,
+    name : "Troll 2",
+    date : "1990",
+    duree : "1h31m",
+    genres : ["Humour","Horreur"],
+    realisateur : "Claudio Fragasso",
+    acteurs : [ "Michael Stephenson", "George Hardy", "Michael Bates"],
+    description:"La famille Waits passe ses vacances dans la ville de Nilebog. Rapidement, leurs vacances tournent au cauchemar, car la ville est infestée de gobelins végétariens qui veulent transformer la famille en plantes pour les manger.",
+    note : "4",
+    image: "Troll_2.jpg"
+  }
+]
