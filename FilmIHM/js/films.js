d94e0dfc1a1b02a503b6f258386eec7483322016
Buisson <colombet.aurelien@etu.unice.fr>
2016-10-24 17:04:04 +0200

    add some films

--- FilmIHM/js/films.js
+++ FilmIHM/js/films.js
@@ -34,5 +34,41 @@ var films = [
     description:"La famille Waits passe ses vacances dans la ville de Nilebog. Rapidement, leurs vacances tournent au cauchemar, car la ville est infestée de gobelins végétariens qui veulent transformer la famille en plantes pour les manger.",
     note : "4",
     image: "Troll_2.jpg"
+  },
+  {
+    id:4,
+    name : "Les Gardiens de la Galaxie",
+    date : "13/08/2014",
+    duree : "2h01m",
+    genres : ["SF","Action"],
+    realisateur : "James Gunn",
+    acteurs : [ "Chris Pratt", "Zoe Saldana", "Dave Bautista"],
+    description:"Peter Quill est un aventurier traqué par tous les chasseurs de primes pour avoir volé un mystérieux globe convoité par le puissant Ronan, dont les agissements menacent l’univers tout entier. Lorsqu’il découvre le véritable pouvoir de ce globe et la menace qui pèse sur la galaxie, il conclut une alliance fragile avec quatre aliens disparates : Rocket, un raton laveur fin tireur, Groot, un humanoïde semblable à un arbre, l’énigmatique et mortelle Gamora, et Drax le Destructeur, qui ne rêve que de vengeance. En les ralliant à sa cause, il les convainc de livrer un ultime combat aussi désespéré soit-il pour sauver ce qui peut encore l’être … ",
+    note : "4",
+    image: "guardians.jpg"
+  },
+  {
+    id:5,
+    name : "Hot Fuzz",
+    date : "18/07/2007",
+    duree : "2h00m",
+    genres : ["Comédie","Action"],
+    realisateur : "Edgar Wright",
+    acteurs : [ "Simon Pegg", "Martin Freeman", "Bill Nighy"],
+    description:"A Londres, le policier Nicholas Angel est le meilleur de son équipe. Tellement bon qu'il fait passer ses collègues pour de simples gardiens de la paix. Le chef de la brigade décide donc de le \"promouvoir\" dans le petit village de Sandford, où il ne se passe rien. Aux côtés du policier local Danny Butterman qui rêve de devenir Mel Gibson, Nicholas règle quelques contraventions sans grand intérêt. Une série de crimes étranges va le remettre dans l'action... ",
+    note : "4",
+    image: "HotFuzz.jpg"
+  },
+  {
+    id:6,
+    name : "H2G2 : le guide du voyageur galactique",
+    date : "17/08/2005",
+    duree : "1h48m",
+    genres : ["SF","Comédie"],
+    realisateur : "Garth Jennings",
+    acteurs : [ "Martin Freeman", "Yasiin Bey", "Sam Rockwell"],
+    description:"Sale journée pour le Terrien Arthur Dent. Sa maison est sur le point d'être rasée par un bulldozer, il découvre que son meilleur ami, Ford Prefect, est un extraterrestre et pour couronner le tout, la Terre va être pulvérisée dans quelques minutes pour faire de la place à une voie express hyperspatiale. Arthur a une chance de survivre, mais il doit pour cela se faire prendre en stop par un vaisseau spatial, avec l'aide de Ford. Sa plus grande aventure va commencer au moment où notre propre monde disparaît... Arthur se jette dans l'inconnu et entame un délirant périple au cours duquel il va découvrir la véritable nature de l'univers. ",
+    note : "4",
+    image: "42.jpg"
   }
 ]
